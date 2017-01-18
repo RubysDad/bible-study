264e4011354798273c835e4658e150c744d34eb8
mandolin <marktravismorales@gmail.com>
2017-01-18 18:40:00 +0000

    gemfile updated

--- app/uploaders/audio_uploader.rb
+++ app/uploaders/audio_uploader.rb
@@ -5,10 +5,10 @@ class AudioUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-if Rails.env.production?
+#if Rails.env.production?
   storage :fog
-else
-  storage :file
+#else
+#  storage :file
 end
 
   # Override the directory where uploaded files will be stored.
--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,6 +5,8 @@ if Rails.env.production?
             :aws_access_key_id => ENV['S3_ACCESS_KEY'],
             :aws_secret_access_key => ENV['S3_SECRET_KEY']
             }
+        config.storage = :fog
+            config.fog_provider = 'fog/aws'
         config.fog_directory = ENV['S3_BUCKET']
     end
 end
